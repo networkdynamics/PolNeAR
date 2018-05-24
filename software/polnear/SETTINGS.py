f73244264b471e2a2871d594fe6baa1e9db0c840
Edward Newell <edward.newell@gmail.com>
2018-05-24 14:42:48 -0400

    add SETTINGS.py file

---
+++ software/polnear/SETTINGS.py
@@ -0,0 +1,5 @@
+import os
+
+CLONE_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+POLNEAR_DATA_DIR = '/home/ndg/users/enewel3/attribution-annotation/data/export'
+METADATA_PATH = os.path.join(POLNEAR_DATA_DIR, 'metadata.tsv')
